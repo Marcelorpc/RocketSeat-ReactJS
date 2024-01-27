8f008e7d5430b83e3dd603370a420fd9832f84d7
marcelorpc <marcelorpchaves@gmail.com>
2024-01-27 17:51:16 -0300

    end of class

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import {Post} from "./Post"
+import { Post } from "./Post"
 
 function App() {
   return (
-    <Post />
+    <div>
+      <Post
+        author="Marcelo Paiva Chaves"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, dolor fugiat optio adipisci error, dignissimos quidem at ad officiis ratione officia et odit reiciendis autem facere iste facilis tempora commodi?"
+      />
+      <Post
+        author="Marcelo Paiva Chaves"
+        content="Segundo Post"
+      />
+    </div>
   )
 }
-
 export default App
