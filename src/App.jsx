16442a74dc2cc53cc29ce049ba94f65b4ba9c8f2
marcelorpc <marcelorpchaves@gmail.com>
2024-01-27 18:39:01 -0300

    css module test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Post } from "./Post"
+import { Header } from "./components/header"
 import './styles.css'
 
 function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author="Marcelo Paiva Chaves"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, dolor fugiat optio adipisci error, dignissimos quidem at ad officiis ratione officia et odit reiciendis autem facere iste facilis tempora commodi?"
