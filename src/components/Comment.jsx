39e4bd04e7917d1ab2b7762bc2890cd99925faf4
marcelorpc <marcelorpchaves@gmail.com>
2024-01-30 14:04:55 -0300

    adjust trash icon size

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,35 @@
-import styles from './Comments.module.css'
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
 
-function Comment() {
+export function Comment() {
   return (
-      <div></div>
+      <div className={styles.comment}>
+        <img src='https://avatars.githubusercontent.com/u/105247533?v=4' />
+
+        <div className={styles.commentBox}>
+          <div className={styles.commentContent}>
+            <header>
+              <div className={styles.authorAndTime}>
+                <strong>Marcelo Paiva Chaves</strong>
+
+                <time title="28 de Janeiro as 11:08h" dateTime="28-01-2024 11:08:30">Publicado ha 30min</time>
+              </div>
+
+              <button title='Deletar comentario'>
+                <Trash size={24} />
+              </button>
+            </header>
+
+            <p>Muito bom, excelente!</p>
+          </div>
+
+          <footer>
+            <button>
+              <ThumbsUp />
+              Aplaudir <span>20</span>
+            </button>
+          </footer>
+        </div>
+      </div>
   )
 }
