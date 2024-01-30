e8bdddd5cb0f567f1def903e1b15817767a41c30
marcelorpc <marcelorpchaves@gmail.com>
2024-01-30 17:00:31 -0300

    add dynamic post system

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,41 @@ import './global.css'
 
 import styles from "./App.module.css"
 
+// author: {avatar_url: "", name: "", role: ""}
+//publishedAt: Date
+// content: ""
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/105247533?v=4",
+      name: "Marcelo Paiva",
+      role: "Web Developer"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '<a href="">jane.design/doctorcare</a>'},
+    ],
+    publishedAt: new Date('2024-01-30 16:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/105247533?v=4",
+      name: "Marcelo Paiva",
+      role: "Web Developer"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '<a href="">jane.design/doctorcare</a>'},
+    ],
+    publishedAt: new Date('2024-01-30 17:00:00')
+  },
+]
+
 function App() {
   return (
     <div>
@@ -14,8 +49,13 @@ function App() {
       <main className={styles.wrapper}>
         <Sidebar />
         <div>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (<Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />)
+          })}
         </div>
       </main>
     </div>
